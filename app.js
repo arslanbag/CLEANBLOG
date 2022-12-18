e4b4de349ed7330d4b63b4fc5caceeabf889c8c2
arslanbag <a.arslanbag@gmail.com>
2022-12-18 16:38:12 +0300

    Clean Blog Template Load

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
+//const ejs = require('ejs')
 const port = 5000;
+app.set("view engine", "ejs")
 
+//Middlewares 
+app.use(express.static('public'));
+
+//Routes
 app.get('/', (req, res) => 
 {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.status(200).send(blog);
+  //const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+  //res.status(200).send(blog);
+  res.render('index')
+});
+
+app.get('/about', (req, res) => 
+{
+  res.render('about')
+});
+
+app.get('/add_post', (req, res) => 
+{
+  res.render('add_post')
+});
+
+app.get('/post', (req, res) => 
+{
+  res.render('post')
 });
 
 app.listen(port, () => 
